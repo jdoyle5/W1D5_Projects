e1881eb436ffb4f9f0a224632e5e65167e9c3b9f
Joey Doyle <j.doyle345@gmail.com>
2017-10-06 11:26:58 -0700

    completed PolyTreeNode.rb

---
+++ trees/lib/00_tree_node.rb
@@ -0,0 +1,51 @@
+class PolyTreeNode
+  attr_reader :value, :children, :parent
+  attr_accessor :children
+
+  def initialize(value, children = [])
+    @value = value
+    @children = children
+    @parent = nil
+  end
+
+  def parent=(parent)
+    @parent.children.delete(self) unless @parent.nil?
+    @parent = parent
+    return parent if parent.nil?
+    if !parent.children.include?(self)
+      parent.children << self
+    end
+  end
+
+  def add_child(child)
+    child.parent= (self)
+  end
+
+  def remove_child(child)
+    if @children.include?(child)
+      child.parent = (nil)
+    else
+      raise "this is not your baby"
+    end
+  end
+
+  def dfs(target_value)
+    return self if @value == target_value
+    @children.each do |child|
+      result = child.dfs(target_value)
+      return result if result
+    end
+    nil
+  end
+
+  def bfs(target_value)
+    queue = [self]
+    until queue.empty?
+      current_node = queue.shift
+      return current_node if current_node.value == target_value
+      queue.concat(current_node.children)
+    end
+    nil
+  end
+
+end
